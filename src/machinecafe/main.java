001548888c54a47cdae84516fe9ac613fde209ef
Guillaume <grue57@yahoo.fr>
2019-02-01 15:01:32 +0100

    ajout BDD machineacafe2

--- src/machinecafe/main.java
+++ src/machinecafe/main.java
@@ -1,18 +1,14 @@
 package machinecafe;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Scanner;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class main {
 	
@@ -33,63 +29,50 @@ public class main {
 		return kb.getChoice(1,7);
 	}
 	
+	public static ArrayList<Ingredient> instantiateIngredients() {
+		ArrayList<Ingredient> listeIngredient = new ArrayList<Ingredient>();
+		try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/machineacafe", "postgres", "postgres")) {
+			Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM public.ingredient");
+            while (resultSet.next()) {
+            	listeIngredient.add(new Ingredient(resultSet.getInt("id"),resultSet.getString("nom"),resultSet.getInt("quantite")));
+            }
+		} catch (SQLException e) {
+            System.out.println("Connection failure.");
+            e.printStackTrace();
+        }
+		return listeIngredient;
+	}
+	
+	public static void updateStocks(ArrayList<Ingredient> liste) {
+		try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/machineacafe", "postgres", "postgres")) {
+			for(Ingredient i : liste) {
+				String sql = "update ingredient set quantite=? where id=?";
+				PreparedStatement stmt = connection.prepareStatement(sql);
+				stmt.setInt(1, i.getQuantite());
+				stmt.setInt(2, i.getId());
+				stmt.executeUpdate();
+			}
+		} catch (SQLException e) {
+            System.out.println("Connection failure.");
+            e.printStackTrace();
+        }
+	}
 	
 	
 	public static void main(String[] args) {
 		
-		try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/machineacafe", "postgres", "postgres")) {
-			 
-            System.out.println("Java JDBC PostgreSQL Example");
-            System.out.println("Connected to PostgreSQL database!");
-            
-            ArrayList<Ingredient> listeIngredient = new ArrayList<Ingredient>();
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM public.ingredient");
-            while (resultSet.next()) {
-            	listeIngredient.add(new Ingredient(resultSet.getString("nom"),resultSet.getInt("quantite")));
-            }
- 
-        } /*catch (ClassNotFoundException e) {
-            System.out.println("PostgreSQL JDBC driver not found.");
-            e.printStackTrace();
-        }*/ catch (SQLException e) {
-            System.out.println("Connection failure.");
-            e.printStackTrace();
-        }
-		
-		//Liste des ingrédients
-		Ingredient cafe = new Ingredient("café", 10);
-		Ingredient chocolat = new Ingredient("chocolat",10);	
-		Ingredient lait = new Ingredient("lait", 10);
-		Ingredient sucre = new Ingredient("sucre", 10);
-		Ingredient the = new Ingredient("thé", 10);
-		ArrayList<Ingredient> listeIngredient = new ArrayList<Ingredient>();
-		listeIngredient.add(cafe);
-		listeIngredient.add(lait);
-		listeIngredient.add(sucre);
-		listeIngredient.add(chocolat);
-		listeIngredient.add(the);
-		
-		//Création chocolat chaud
-		Map<Ingredient,Integer> z = new HashMap<Ingredient,Integer>();
-		z.put(lait, 2);
-		z.put(chocolat,3);
-		Boisson boisson1 = new Boisson("chocolat chaud", 1, z);
-		//Création capuccino
-		z = new HashMap<Ingredient,Integer>();
-		z.put(cafe,2);
-		z.put(lait, 1);
-		Boisson boisson2 = new Boisson("capuccino", 2, z);
-		//Création cafe
-		z = new HashMap<Ingredient,Integer>();
-		z.put(cafe, 3);
-		Boisson boisson3 = new Boisson("café", 1, z);
+		ArrayList<Ingredient> listeIngredient = instantiateIngredients();		
+		int posSucre = 0;
+		for(int i = 0;i<listeIngredient.size();i++) {
+			if(listeIngredient.get(i).getNom().equals("sucre")) {
+				posSucre = i;
+			}
+		}
+
 		
 		//Création de la machine à café
 		List<Boisson> listeBoisson = new ArrayList<Boisson>();
-		listeBoisson.add(boisson1);
-		listeBoisson.add(boisson2);
-		listeBoisson.add(boisson3);
 		MachineACafe machine = new MachineACafe(listeBoisson,listeIngredient);
 		
 		boolean continuer = true;
@@ -99,18 +82,23 @@ public class main {
 			int choix = menu();
 			switch (choix) {
 				case 1:
+					if(machine.getListe().size() == 0) {
+						System.out.println("Il n'y a pas de boisson dans la machine");
+						break;
+					}
 					Boisson boisson = machine.choisirBoisson();
 					if(monnaie == 0 || monnaie < boisson.getPrix()) {
 						monnaie += kb.insertMonnaie();
 					}
 					int qteSucre = kb.getSucre();
-					if (sucre.getQuantite() < qteSucre) {
+					if (listeIngredient.get(posSucre).getQuantite() < qteSucre) {
 						System.out.println("Pas assez de sucre");
 						break;
 					}
 					if(machine.acheterBoisson(boisson, monnaie)) {
 						monnaie -= boisson.getPrix();
-						sucre.setQuantite(sucre.getQuantite()-qteSucre);
+						listeIngredient.get(posSucre).setQuantite(listeIngredient.get(posSucre).getQuantite()-qteSucre);
+						updateStocks(listeIngredient);
 					}
 					break;
 				case 2:
